317dbe3fbe078e79a89ca74337dcc8c6c07da7e2
khan9t2zishan <106923884+khan9t2zishan@users.noreply.github.com>
2022-12-25 17:37:21 +0530

    Add files via upload

---
+++ 23array.c
@@ -0,0 +1,13 @@
+//Program to insert an element in an array
+#include<stdio.h>
+void main()
+{
+    int n,a[n],i;                        //n is the size of the array
+    printf("size of array:");
+    scanf("%d",&n);
+    printf("input element:");
+    for(i=0;i<=n;i++)
+    {
+    scanf("%d",&a[n]);
+    }
+}
