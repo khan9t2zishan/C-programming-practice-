523576da5cb6c192f7885c884d8a5752868a006c
khan9t2zishan <106923884+khan9t2zishan@users.noreply.github.com>
2022-12-25 23:06:00 +0530

    Update 23array.c

--- 23array.c
+++ 23array.c
@@ -1,13 +1,24 @@
 //Program to insert an element in an array
 #include<stdio.h>
-void main()
+void main() 
 {
-    int n,a[n],i;                        //n is the size of the array
-    printf("size of array:");
-    scanf("%d",&n);
-    printf("input element:");
-    for(i=0;i<=n;i++)
-    {
-    scanf("%d",&a[n]);
-    }
+   int arr[30], element, num, i, location;
+   printf("\nEnter no of elements :");
+   scanf("%d", &num);
+   for (i = 0; i < num; i++) 
+   {
+      scanf("%d", &arr[i]);
+   }
+   printf("\nEnter the element to be inserted :");
+   scanf("%d", &element);
+   printf("\nEnter the location");
+   scanf("%d", &location);
+   for (i = num; i >= location; i--)                     //Create space at the specified location
+   {
+      arr[i] = arr[i - 1];
+   }
+   num++;
+   arr[location - 1] = element;
+   for (i = 0; i < num; i++)                             //Print out the result of insertion
+      printf("\n %d", arr[i]);
 }
