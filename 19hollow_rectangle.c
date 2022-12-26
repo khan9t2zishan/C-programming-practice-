e0eb95aaa24bcede16e02e2b2d5f1119962f454d
khan9t2zishan <106923884+khan9t2zishan@users.noreply.github.com>
2022-12-26 21:29:07 +0530

    Add files via upload

---
+++ 19hollow_rectangle.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+    int i, j, rows, columns,a,num;
+     
+    printf(" \nPlease Enter the Number of Rows : ");
+    scanf("%d", &rows);
+    
+    printf(" \nPlease Enter the Number of Columns : ");
+    scanf("%d", &columns);
+    for(i = 1; i <= rows; i++)
+    {
+    	for(j = 1; j <= columns; j++)
+		{
+			if(i == 1 || i == rows || j == 1 || j == columns)
+			{
+				printf("*");
+			}
+			else
+			{
+				printf(" ");
+			}       	
+        }
+        printf("\n");
+    }
+    return 0;
+}
