35364867a3e93aefce3485e12e459b3335026938
khan9t2zishan <106923884+khan9t2zishan@users.noreply.github.com>
2022-12-26 22:36:09 +0530

    Update 24transposematrix.c

--- 24transposematrix.c
+++ 24transposematrix.c
@@ -2,9 +2,10 @@
 #include<stdio.h>
 void main()
 {
-    int i,j,m,n,A[100][100];
+    int i,j,m,n;
     printf("no. of rows and columns:");
     scanf("%d %d",&m,&n);
+    int A[m][n];
     printf("elements for the matrix:");
     for(i=0;i<m;i++)
     {
@@ -23,9 +24,9 @@ void main()
     printf("\n");
     }
     printf("Transpose:\n");
-    for(i=0;i<m;i++)
+    for(i=0;i<n;i++)
     {
-    for(j=0;j<n;j++)
+    for(j=0;j<m;j++)
     {
     printf(" %d ",A[j][i]);
     }
