a1413bb83e3a78f5e1220c76210c928254969707
khan9t2zishan <106923884+khan9t2zishan@users.noreply.github.com>
2022-12-27 22:06:59 +0530

    Add files via upload

---
+++ 25Integer-Roman.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+ void int2roman(int num)
+{
+    int integer[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};   //base values
+    char *roman[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};  //roman symbols
+    int i = 0;
+    while(num>0)                               //repeat process until num is not 0
+    {                                        
+        while(num/integer[i])                //first base value that divides num is largest base value
+        {                                    
+            printf("%s",roman[i]);           //print roman symbol equivalent to largest base value
+            num -= integer[i];               //subtract largest base value from num
+        }
+        i++;                                 //move to next base value to divide num
+    }
+}
+int main()
+{
+    int num=14;                            //enter any integer value to convert it into roman
+    printf("Input the integer:");
+    scanf("%d",&num);
+    int2roman(num);
+    
+}
