3a11c65c74b376d42e62b136bb2db367aec5d93f
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2022-12-28 21:32:21 +0530

    Update 27Number_search.c

--- 27Number_search.c
+++ 27Number_search.c
@@ -5,16 +5,16 @@ void main()
     printf("Enter the size of the array:");
     scanf("%d",&m);
     int A[m];
-    printf("Enter the %d elements of the array:",m);
+    printf("Enter the %d elements of the array:\n",m);
     for(i=0;i<m;i++)
     scanf("%d",&A[i]);
     printf("Array A:\n");
-    for(i=1;i<+m;i++)
+    for(i=0;i<m;i++)
     printf("%d ",A[i]);
     printf("\nEnter the number you want to search:");
     scanf("%d",&n);
     printf("Number %d found at position:\n",n);
     for(i=0;i<m;i++)
     if(A[i]==n)
-    printf("%d ",i);
+    printf("%d ",i+1);
 }
