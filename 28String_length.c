40cc179eec4c86700f318faae70a4e31fd7cbb3f
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2022-12-29 20:25:15 +0530

    Update 28String_length.c

--- 28String_length.c
+++ 28String_length.c
@@ -6,10 +6,9 @@ void main()
     int m;
     printf("Enter the size of the Array:");
     scanf("%d",&m);
-    int A[m],i;
+    char A[m],i;
     printf("Enter the string:");
     scanf("%s",A);
-    printf("%s",A);
     strlen(A);
-    printf("\nLength of the string is: %d",strlen(A)); 
+    printf("\nLength of the string is: %d",strlen(A));
 }
